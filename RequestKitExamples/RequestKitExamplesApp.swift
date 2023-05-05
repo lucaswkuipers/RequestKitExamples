ee29cd5e2e96787b379fd048039962066169ee51
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2023-05-05 14:40:47 -0300

    Initial Commit

---
+++ RequestKitExamples/RequestKitExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestKitExamplesApp.swift
+//  RequestKitExamples
+//
+//  Created by Lucas Werner Kuipers on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RequestKitExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
